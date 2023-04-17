0024f925c6405e0e5d02faaae9b3237e626a52a8
Svetlana <rus_lana@bk.ru>
2023-04-17 13:05:05 +0500

    Added test commit

--- src/test/java/CardDelveryOrderTest.java
+++ src/test/java/CardDelveryOrderTest.java
@@ -1,2 +1,38 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class CardDelveryOrderTest {
+    @BeforeEach
+    public void setup () {
+        open("http://localhost:9999");
+    }
+    public String generateDate(String pattern) {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    @Test
+    void shouldSendForm() {
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = true;
+        $ (LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        $("[data-test-id='city'] input").setValue("Уфа");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        String date = generateDate( "dd.MM.yyyy");
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue("Карабасова Земфира-Лукреция");
+        $("[data-test-id='phone'] input").setValue("+79111111111");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.ownText(date));
+    }
 }
